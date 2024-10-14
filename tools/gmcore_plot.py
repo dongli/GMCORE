5a09cdb9c9b171d12af51ffbe4bd9638b642030c
Li Dong <dongli@lasg.iap.ac.cn>
2024-10-14 13:33:16 +0800

    Minor changes

--- tools/gmcore_plot.py
+++ tools/gmcore_plot.py
@@ -1,6 +1,7 @@
 import argparse
 import xarray as xr
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.ticker as ticker
@@ -16,13 +17,15 @@ def vinterp(zi, var, zo):
 	res = xr.DataArray(res, coords=var.coords, dims=var.dims)
 	return res
 
-def plot_contour(ax, lon, lat, var, cmap=None, levels=None, left_string=None, right_string=None, with_grid=True):
+def plot_contour(ax, lon, lat, var, cmap=None, levels=None, left_string=None, right_string=None, with_grid=True, font_size=8):
 	if left_string is not None:
 		ax.set_title(left_string)
+		# How to set title font size?
+		ax.title.set_fontsize(font_size)
 	else:
 		ax.set_title('')
 	var, lon = cutil.add_cyclic_point(var, coord=lon)
-	im = ax.contourf(lon, lat, var, transform=ccrs.PlateCarree(), cmap=cmap, levels=levels)
+	im = ax.contourf(lon, lat, var, transform=ccrs.PlateCarree(), cmap=cmap, levels=levels, extend='both')
 	ax.contour(lon, lat, var, transform=ccrs.PlateCarree(), levels=levels, linewidths=0.2, colors='k')
 	if with_grid:
 		gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, color='gray', alpha=0.5, linestyle='--')
@@ -30,12 +33,15 @@ def plot_contour(ax, lon, lat, var, cmap=None, levels=None, left_string=None, ri
 		gl.right_labels = False
 		gl.xformatter = ccrs.cartopy.mpl.gridliner.LONGITUDE_FORMATTER
 		gl.yformatter = ccrs.cartopy.mpl.gridliner.LATITUDE_FORMATTER
+		gl.xlabel_style = {'size': font_size}
+		gl.ylabel_style = {'size': font_size}
 	cax = make_axes_locatable(ax).append_axes('right', size='5%', pad=0.05, axes_class=plt.Axes)
 	cbar = plt.colorbar(im, cax=cax, orientation='vertical')
 	formatter = ticker.ScalarFormatter(useMathText=True)
 	formatter.set_scientific(True)
 	formatter.set_powerlimits((0, 0))
 	cbar.formatter = formatter
+	cbar.ax.tick_params(labelsize=font_size)
 	cbar.update_ticks()
 
 def plot_time_series(ax, time, var, color='black'):
