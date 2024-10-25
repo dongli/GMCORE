b3ae6fa5a39af4e91863791787a328efcb8ae7c8
Li Dong <dongli@lasg.iap.ac.cn>
2024-10-25 09:14:21 +0800

    Add with_contour arg in gmcore_plot.py

--- tools/gmcore_plot.py
+++ tools/gmcore_plot.py
@@ -19,7 +19,7 @@ def vinterp(zi, var, zo):
 	res = xr.DataArray(res, coords=var.coords, dims=var.dims)
 	return res
 
-def plot_contour(ax, lon, lat, var, cmap=None, levels=None, left_string=None, right_string=None, with_grid=True, font_size=12, add_cyclic_point=True, use_scientific=False, show_lat_labels=True, contour_linewidth=0.5):
+def plot_contour(ax, lon, lat, var, cmap=None, levels=None, left_string=None, right_string=None, with_grid=True, font_size=12, add_cyclic_point=True, use_scientific=False, show_lat_labels=True, with_contour=True, contour_linewidth=0.5):
 	if left_string is not None:
 		ax.set_title(left_string)
 		# How to set title font size?
@@ -28,7 +28,7 @@ def plot_contour(ax, lon, lat, var, cmap=None, levels=None, left_string=None, ri
 		ax.set_title('')
 	if add_cyclic_point: var, lon = cutil.add_cyclic_point(var, coord=lon)
 	im = ax.contourf(lon, lat, var, transform=ccrs.PlateCarree(), cmap=cmap, levels=levels, extend='both')
-	ax.contour(lon, lat, var, transform=ccrs.PlateCarree(), levels=levels, linewidths=contour_linewidth, colors='k')
+	if with_contour: ax.contour(lon, lat, var, transform=ccrs.PlateCarree(), levels=levels, linewidths=contour_linewidth, colors='k')
 	if with_grid:
 		gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, color='gray', alpha=0.5, linestyle='--')
 		gl.top_labels = False
